b5c93c030cb9595738754d9d21138ea5dd323d58
yemam <youssefemam04@gmail.com>
2023-05-26 09:30:02 +0000

    finefine

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,23 @@
 from flask import render_template
 from flask import Flask
+import mysql.connector
+import pandas as pd
 
+#Connect to mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Serial_Killers"
+)
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('hello.html', name='Decks')
+@app.route('/assasins')
+def assasinslist():
+    mycursor.execute("SELECT * FROM assasins")
+    myresult = mycursor.fetchall()
+    return render_template('assasins.html', assasins=myresult)
